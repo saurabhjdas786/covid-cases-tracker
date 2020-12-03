8069aea9552cf57d048171376466c35dd2ffaffb
Saurabh Das <saurabhjdas786@gmail.com>
2020-12-03 22:49:11 +0530

    Improve design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
-
 import Cards from "./components/Cards/Cards";
 import Chart from "./components/Chart/Chart";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
-
 import styles from "./App.module.css";
-
 import { fetchData } from "./api";
+import coronaBanner from "./images/corona-banner2.jpg";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Grid, Paper, Switch, Typography } from "@material-ui/core";
+import bulbOn from "./images/icon/light.svg";
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   const [data, setData] = useState({});
   const [selectedCountry, setSelectedCountry] = useState("");
+
+  const darkThemeUI = createMuiTheme({
+    palette: {
+      type: "dark",
+      background: {
+        paper: "#1a1919",
+      },
+      grey: "red",
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+
+  const lightThemeUI = createMuiTheme({
+    palette: {
+      type: "light",
+      background: {
+        paper: "#fff",
+      },
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
 
   useEffect(() => {
     (async () => {
@@ -22,11 +49,40 @@ function App() {
   useEffect(() => {}, [selectedCountry]);
 
   return (
-    <div className={styles.container}>
-      <Cards {...data} />
-      <CountryPicker setSelectedCountry={setSelectedCountry} />
-      <Chart {...data} selectedCountry={selectedCountry} />
-    </div>
+    <ThemeProvider theme={isDarkTheme ? darkThemeUI : lightThemeUI}>
+      <Paper>
+        <div className={styles.container}>
+          <img
+            className={styles.coronaBanner}
+            src={coronaBanner}
+            alt="Coronavirus Banner"
+          />
+          <Typography component="div">
+            <Grid component="label" container alignItems="center" spacing={1}>
+              <Grid item>
+                <img
+                  src={bulbOn}
+                  alt="light theme on"
+                  width="35px"
+                  height="35px"
+                />
+              </Grid>
+              <Grid item>
+                <Switch
+                  checked={isDarkTheme}
+                  onChange={() => {
+                    setIsDarkTheme(!isDarkTheme);
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </Typography>
+          <Cards {...data} />
+          <CountryPicker setSelectedCountry={setSelectedCountry} />
+          <Chart {...data} selectedCountry={selectedCountry} />
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
